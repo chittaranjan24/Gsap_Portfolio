8c7cccf5085c6ae85ccab0ef02267ad4aa2d4e71
chittaranjan24 <chittaranjanshit2@gmail.com>
2025-07-26 22:41:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className='relative w-screen min-h-screen overfllow-x-auto'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
