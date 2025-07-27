83417b9a26f8f1f39c1eb3256c6b775d8ada1dfd
chittaranjan24 <chittaranjanshit2@gmail.com>
2025-07-28 00:39:38 +0530

    complete animated textLine

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,12 @@
 import { useRef } from "react"
+import {AnimatedTextLines} from "../components/AnimatedTextLines"
 
 const Hero = () => {
     const contextRef = useRef(null)
     const headerRef = useRef(null)
+    const aboutText = `I help growing brands and startups gain an
+     unfait advantage through premium
+     results driven webs/apps`
 
   return (
     <section id="home" className="flex flex-col justify-end min-h-screen">
@@ -13,17 +17,14 @@ const Hero = () => {
                 <div ref={headerRef} className="flex flex-col justify-center gap-0.2 pt-16 sm:gap-16">
                     <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">404 No Bugs Found</p>
                     <div className="px-10">
-                        <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:text-black">Chittaranjan</h1>
+                        <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase banner-text-responsive sm:gap-16 md:block">Chittaranjan</h1>
                     </div>
                 </div>
             </div>
             <div className="relative px-10 text-black">
                 <div className="absolute inset-x-0 border-t-2" />
                 <div className="py-12 sm:py-16 text-end">
-                    <p className="font-light uppercase value-text-responsive">
-                        I help growing brands and startups gain an unfait advantage
-                        through premium results driven webs/apps
-                    </p>
+                    <AnimatedTextLines text={aboutText} className="font-light uppercase value-text-responsive" />
                 </div>
             </div>
         </div>
